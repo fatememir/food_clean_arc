ccd7483b49288c4f8756607a8d772a9f4329a634
fateme <fa.mirr74@gmail.com>
2023-07-10 16:33:39 +0330

    implement event class

---
+++ lib/features/get_recipes/presentation/bloc/bloc.dart
@@ -0,0 +1,4 @@
+
+export 'get_recipes_bloc.dart';
+export 'get_recipes_event.dart';
+export 'get_recipes_state.dart';
---
+++ lib/features/get_recipes/presentation/bloc/get_recipes_event.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class GetRecipesBlocEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class GetRecipes extends GetRecipesBlocEvent {
+
+  @override
+  List<Object> get props => [];
+}
+
